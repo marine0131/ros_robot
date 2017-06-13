952151abcad08a0aaf9e7486bd9f3f21c461d647
Haijun Wang <wanghj@woosiyuan.com>
2017-06-13 09:44:49 +0800

    delete build folder

--- build/sick_tim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/whj/ros/src/sick_tim
-# Build directory: /home/whj/ros/build/sick_tim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/xunjian_nav/CMakeFiles/xunjian_nav_generate_messages.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/xunjian_nav_generate_messages"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/xunjian_nav_generate_messages.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
