605a06a890fde79a7c97f99169de93d3355fd23e
Haijun Wang <wanghj@woosiyuan.com>
2017-07-06 13:41:22 +0800

    update nav_goal

--- src/nav_goal/src/nav_goal.cpp
+++ src/nav_goal/src/nav_goal.cpp
@@ -2,8 +2,57 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-int goal_flag = 0;
+
+class Goals {
+	public:
+		Goals(){
+		}
+		void set_alias(string a){
+			alias = a;
+		}
+		void set_frame(string frame_id){
+			goal.target_pose.header.frame_id = frame_id;
+		}
+		move_base_msgs::MoveBaseGoal get_goal(){
+			return goal;
+		}
+		void set_position(double x, double y, double z ){
+			goal.target_pose.pose.position.x = x;
+			goal.target_pose.pose.position.y = y;
+			goal.target_pose.pose.position.z = z;
+		}
+	private:
+		string alias;
+		move_base_msgs::MoveBaseGoal goal;
+};
+#define DOOR 0
+#define SERVER 1
+#define BOSS 2
+#define WC 3
+#define ROOM 4
+#define GATE 5
+
+Goals goals[20];
+goals[0].set_frame("map");
+goals[0].set_position(1.45884692669,0.41893029213,0);
+goals[0].set_quat(0,0,0.380679358544,0.924707102805);
+goals[1].set_frame("map");
+goals[1].set_position(6.51145744324,-2.35952734947,0);
+goals[1].set_quat(0,0,0.909674541736,-0.415321836794);
+goals[2].set_frame("map");
+goals[2].set_position(-4.2894,-7.019709,0);
+goals[2].set_quat(0,0,0.954226403507,-0.299085223391);
+goals[3].set_frame("map");
+goals[3].set_position(11.3937091827,0.897811412811,0);
+goals[3].set_quat(0,0,0.424219369561,0.905559454972);
+goals[4].set_frame("map");
+goals[4].set_position(-0.576218009,1.0124691725,0);
+goals[4].set_quat(0,0,-0.3492525274,0.9370286399);
+goals[5].set_frame("map");
+goals[5].set_position(3.52077460289,-6.87122631073,0);
+goals[5].set_quat(0,0,-0.341993935225,0.939702159341);
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "nav_goal");
@@ -14,66 +63,12 @@ int main(int argc, char** argv){
 	while(!ac.waitForServer(ros::Duration(5.0))){
 		ROS_INFO("Waiting for the move_base action server to come up");	
 	}
-	
-	move_base_msgs::MoveBaseGoal goal;
-	
-	goal.target_pose.header.frame_id = "map";
-	goal.target_pose.header.stamp = ros::Time::now();
-	
+	int p[2]={DOOR, ROOM};
+	int ii = 0;
 	while(ros::ok()){
-		switch(goal_flag){
-			case 0://door
-			goal_flag =6;
-			goal.target_pose.pose.position.x = 1.45884692669;
-			goal.target_pose.pose.position.y = 0.41893029213;
-			goal.target_pose.pose.orientation.z = 0.380679358544;
-			goal.target_pose.pose.orientation.w = 0.924707102805;
-			break;
-			case 1:	//server
-			goal_flag ++;
-			goal.target_pose.pose.position.x = 6.51145744324;
-			goal.target_pose.pose.position.y = -2.35952734947;
-			goal.target_pose.pose.orientation.z = 0.909674541736;
-			goal.target_pose.pose.orientation.w = -0.415321836794;
-			break;
-			case 2://boss
-			goal_flag ++;
-			goal.target_pose.pose.position.x = -4.2894;
-			goal.target_pose.pose.position.y = -7.019709;
-			goal.target_pose.pose.orientation.z = 0.954226403507;
-			goal.target_pose.pose.orientation.w = -0.299085223391;
-			break;
-			case 3://wc
-			goal_flag ++;
-			goal.target_pose.pose.position.x = 11.3937091827;
-			goal.target_pose.pose.position.y = 0.897811412811;
-			goal.target_pose.pose.orientation.z = 0.424219369561;
-			goal.target_pose.pose.orientation.w = 0.905559454972;
-			break;	
-			case 4:	//server
-			goal_flag ++;
-			goal.target_pose.pose.position.x = 6.51145744324;
-			goal.target_pose.pose.position.y = -2.35952734947;
-			goal.target_pose.pose.orientation.z = 0.909674541736;
-			goal.target_pose.pose.orientation.w = -0.415321836794;
-			break;
-			case 5://door
-			goal_flag ++;
-			goal.target_pose.pose.position.x = 1.45884692669;
-			goal.target_pose.pose.position.y = 0.41893029213;
-			goal.target_pose.pose.orientation.z = 0.380679358544;
-			goal.target_pose.pose.orientation.w = 0.924707102805;
-			break;
-			case 6://zero			
-			goal_flag = 0 ;
-			goal.target_pose.pose.position.x = -0.576218009;
-			goal.target_pose.pose.position.y = 1.0124691725;
-			goal.target_pose.pose.orientation.z = -0.3492525274;
-			goal.target_pose.pose.orientation.w = 0.9370286399;
-			break;
-		}
-			
-		ac.sendGoal(goal);
+		if (ii>=2) ii = 0;
+		ac.sendGoal(goals[p[ii].get_goal());
+		ii ++;	
 		ac.waitForResult();
 		if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
 			ROS_INFO("goal reached");
